0275b41122b31d71edce2afb868e3fca2afc839e
quickdevita <quickdevita@gmail.com>
2025-02-10 14:34:59 +0100

    Aggiunta funzione zone lavori

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// Inizializzazione della mappa con Leaflet.js
-var map = L.map('map').setView([38.1157, 13.3615], 13); // Coordinate di Palermo (modifica se necessario)
+// Inizializzazione della mappa
+var map = L.map('map').setView([38.1157, 13.3615], 13); // Palermo
 
-// Aggiungi la mappa di OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Aggiunta della mappa satellitare Esri
+var esriLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri'
 }).addTo(map);
+
+// Caricamento dei dati dal file JSON
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(zone => {
+      var polygon = L.polygon(zone.coordinates, {
+        color: zone.color,
+        fillColor: zone.color,
+        fillOpacity: 0.5
+      }).addTo(map);
+
+      // Popup con le informazioni del lavoro
+      polygon.bindPopup(`<b>${zone.name}</b><br>${zone.info}`);
+    });
+  })
+  .catch(error => console.error("Errore nel caricamento dei dati:", error));

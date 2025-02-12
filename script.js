bccb42d494d95143801f7f1876932ec1562d3174
quickdevita <quickdevita@gmail.com>
2025-02-12 20:25:35 +0100

    Automated commit 12/02/2025 20:25:35,68

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ fetch('data.json')
       var polygon = L.polygon(zone.coordinates, {
         color: zone.color,
         fillColor: zone.color,
-        fillOpacity: 0.5,
-        weight: 2
+        fillOpacity: 0.3,
+        weight: 1
       }).addTo(map);
 
       polygon.on('click', () => openModal(zone.name, zone.info));

f302cba445913190044ca14d0a9c1d2c90f60150
quickdevita <quickdevita@gmail.com>
2025-02-13 03:56:41 +0100

    Automated commit 13/02/2025  3:56:40,97

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function aggiornaPosizionePopup() {
 
   if (isMobile) {
       // Su smartphone: popup alla SINISTRA del punto esclamativo, leggermente più in alto
-      popup.style.left = `${rect.left - popup.offsetWidth - 10}px`; // Sposta a sinistra
+      popup.style.left = `${rect.left - popup.offsetWidth - 15}px`; // Sposta a sinistra
       popup.style.top = `${rect.top - 120}px`; // Sposta più in alto
       popup.classList.add("popup-left");
       popup.classList.remove("popup-down");

a2d41de609868caf6660d110c18f0fb1f1c7c08e
quickdevita <quickdevita@gmail.com>
2025-02-22 16:25:09 +0100

    Automated commit 22/02/2025 16:25:09,01

--- script.js
+++ script.js
@@ -630,6 +630,14 @@ function openSettingsModal() {
   alert('Sezione impostazioni aperta!');
 }
 
+function openSettingsModal() {
+  document.getElementById('settings-modal').style.display = 'block';
+}
+
+function closeSettingsModal() {
+  document.getElementById('settings-modal').style.display = 'none';
+}
+
 
 
 // ==========================
